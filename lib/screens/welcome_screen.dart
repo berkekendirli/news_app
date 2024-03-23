3dd679d01acdbc33c668d28378d5077415d22c68
berkekendirli <efekndrl@gmail.com>
2024-03-24 02:11:50 +0300

    minimalui

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -15,109 +15,109 @@ class WelcomePage extends StatelessWidget {
           decoration: gradientBackground(),
           child: Center(
             child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset('assets/images/news.png', width: 300),
-                  const SizedBox(height: 20),
-                  Text(
-                    'Welcome to the home of news!',
-                    style: GoogleFonts.lobster(
-                      color: const Color.fromARGB(255, 0, 0, 0),
-                      fontSize: 30,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 50),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterPage(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 39.313),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset('assets/images/news.png', width: 300),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Welcome to the home of news!',
+                      style: GoogleFonts.lobster(
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 33,
                       ),
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      backgroundColor: const Color.fromARGB(255, 44, 44, 44),
-                      foregroundColor: Colors.white,
+                      textAlign: TextAlign.center,
                     ),
-                    child: SizedBox(
-                      width: 150,
-                      height: 50,
-                      child: Center(
-                        child: Text(
-                          'Sign Up',
-                          style: GoogleFonts.overpass(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                    const SizedBox(height: 50),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterPage(),
                           ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(8),
                         ),
                         padding: const EdgeInsets.symmetric(horizontal: 20),
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.black),
-                    child: SizedBox(
-                      width: 150,
-                      height: 50,
-                      child: Center(
-                        child: Text(
-                          'Sign In',
-                          style: GoogleFonts.overpass(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: const Color.fromARGB(255, 44, 44, 44),
+                        backgroundColor: const Color.fromARGB(255, 44, 44, 44),
+                        foregroundColor: Colors.white,
+                      ),
+                      child: SizedBox(
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'Sign Up',
+                            style: GoogleFonts.overpass(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      backgroundColor: const Color.fromARGB(255, 163, 60, 60),
-                      foregroundColor: Colors.white,
-                    ),
-                    child: SizedBox(
-                      width: 150,
-                      height: 50,
-                      child: Center(
-                        child: Text(
-                          'Skip',
-                          style: GoogleFonts.overpass(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black),
+                      child: SizedBox(
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'Sign In',
+                            style: GoogleFonts.overpass(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: const Color.fromARGB(255, 44, 44, 44),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        backgroundColor: const Color.fromARGB(255, 163, 60, 60),
+                        foregroundColor: Colors.white,
+                      ),
+                      child: SizedBox(
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'Skip',
+                            style: GoogleFonts.overpass(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
